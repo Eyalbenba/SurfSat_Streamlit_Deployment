8f835684233496585fc1e3cbfcfad34ad61dd834
Eyalbenba <126380038+Eyalbenba@users.noreply.github.com>
2024-09-16 12:09:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ import random
 MODEL_PATH = r'trained_model.pkl'
 
 # Path to the default image displayed at the top
-DEFAULT_IMAGE_PATH = r'data/Streamlit_open_image.jpg'
+DEFAULT_IMAGE_PATH = r'data/Photos Try.jpg'
 
 # Path to the directory containing sample images
 SAMPLE_IMAGE_DIR = r'data/Sample_Photos'  
